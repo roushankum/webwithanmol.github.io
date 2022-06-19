b03a05d9eaff95acd0520b77d30114791000b465
Anmol <103110623+roushankum@users.noreply.github.com>
2022-06-19 11:33:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+ Let menu = document.querySelector('#menu-icon');
+ Let navbar = document.querySelector('#menu-icon');
+
+ menu.oneclick = () => {
+     menu.classList.toggle('bx-x');
+     navbar.classList.toggle('')
+ }
